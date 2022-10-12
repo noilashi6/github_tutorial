9b1e303a7ceab159a1f91c754d5065a3c592de8e
noila <ibodullayevanoila07@gmail.com>
2022-10-12 17:14:53 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,193 @@
+// function TruFalse (whatDoYouThinkIsItTue){
+//   if (whatDoYouThinkIsItTue) {
+//     return "yes true";
+//   }
+//  return "no false";
+// }
+// console.log(TruFalse(false));
+// console.log(TruFalse(true));
+// console.log(TruFalse(false));
+// console.log(TruFalse(true));
+// var number = 66 / 3;
+// console.log(number);
+
+// var remainder;
+// remainder = 11 % 3;
+// console.log(remainder++)
+
+// var myStr = "I am a\"double quoted\"string inside\"double quotes"
+// console.log(myStr)
+// var link = '<a href="http://www.example.com" target="_blank" >link</a>';
+// console.log(link);
+
+
+// var myStr = "my name is noila." + "my surname is ibodullayeva.";
+// console.log(myStr);
+
+// var myTsr = "this is a first sentences "
+// myTsr += " this is a second sentences"
+// console.log(myTsr);
+
+// var MyName = "noila";
+// var MyTsr = "my name is " + MyName + " and i am a student";
+// console.log(MyTsr);
+
+// var someAdjective = "beautiful";
+// var str = " i am  ";
+// str += someAdjective;
+// console.log(str);
+
+// var lastNameLenght = "";
+// var lastName = "lovelace"
+// // lastNameLenght = lastName.length;
+// // console.log(lastNameLenght);
+// lastNameLenght = lastName[0];
+// console.log(lastNameLenght);
+
+// var kkm = "jello world";
+// kkm = "hello world";
+// console.log(kkm);
+
+// function wordBlanks(myNoun, myAdjective, myVerb, myAdverb){
+//   var result ="";
+//   result += "the " +  myAdjective + " " + myNoun + " " + myVerb + " to the store " +  myAdverb;
+//   return result;
+// }
+// console.log(wordBlanks("dog", "big", "run", "quickly"));
+
+
+// var myGlobal = 10;
+// function fun1(){
+
+// }
+// function fun2(){
+//   var output ="";
+//   if (typeof myGlobal != "undefined"){
+//     output += "myGlobal: " + myGlobal;
+//   }
+//   if (typeof oopsGlobal != "undefined"){
+//     output += "oopsGlobal: " + oopsGlobal;
+//     }
+//     console.log(output)
+// }
+// fun1();
+// fun2();
+
+// function TruFalse (whatDoYouThinkIsItTue){
+//   if (whatDoYouThinkIsItTue) {
+//     return "yes true";
+//   }
+//  return "no false";
+// }
+// console.log(TruFalse(false));
+// console.log(TruFalse(true));
+// console.log(TruFalse(false));
+// console.log(TruFalse(true));
+
+// function truee (val){
+//   if (val > 12){
+//     return "yes it is true";
+//   }
+//   return "no it is false";
+// }
+// console.log(truee(12));
+// console.log(truee(9));
+// console.log(truee(24));
+// function TestElse(val){
+//   var result = "";
+//   if (val>10){
+//     return  "bigger than5";
+//   } else if (val <5){
+//     return  "5 or smaller";
+//   } else {
+//     return "between 5 and 10"
+//   }
+    
+ 
+// }
+// console.log(TestElse(7));
+
+// function caseIf(val){
+//   var answer = "";
+//   switch(val){
+//       case 1:
+//       answer = "alphe";
+//       break;
+//       case 2:
+//       answer = "betta";
+//   }  
+  
+//   return answer;
+// }
+// console.log(caseIf(8));
+
+
+// function newOne(val){
+//   var answer = "";
+//   switch(val){
+//     case 1:
+//     case 2:
+//     case 3:
+//       answer = "low";
+//       break;
+//     case 4:
+//     case 5:
+//     case 6:
+//       answer = "med";
+//       break;
+//     case 7:
+//     case 8:
+//     case 9:
+//       answer = "high";
+//       break;
+//     default:
+//       answer = "incorrect";
+//       break;
+    
+
+//   }
+//   return answer;
+// }
+// console.log(newOne(90))
+
+
+
+
+// function chainToSwitch(val){
+//   var answer = "";
+//   switch(val){
+//     case "bob":
+//       answer = "marley";
+//       break;
+//   case 42:
+//       answer = "the answer";
+//       break;
+//   case 1:
+//       answer = "there is no #1";
+//       break;
+//   case 99:
+//     answer="missed e by this much";
+//     break;
+//   case 7:
+//     answer = "ate nine";
+//     break;
+//   }
+//   return answer ;
+// }
+// console.log(chainToSwitch(7));
+
+// function isLess(a, b){
+//   return a< b;
+// }
+// console.log(isLess(95, 78))
+
+var age = prompt("yoshingiz");
+
+if ( age > 18) {
+  alert("kiravering")
+} else {
+  alert("yoshingiz kam")
+}
+
+
+//alert( "foydalanuvchining ismi: " + firstName + " \n va uning yoshi: " + age + "/\n yaxshi ko'rgan kinosi: " + kino);
---
+++ project/app.js
@@ -0,0 +1,100 @@
+// var number = 66 / 3;
+// console.log(number);
+
+// var remainder;
+// remainder = 11 % 3;
+// console.log(remainder++)
+
+// var myStr = "I am a\"double quoted\"string inside\"double quotes"
+// console.log(myStr)
+// var link = '<a href="http://www.example.com" target="_blank" >link</a>';
+// console.log(link);
+
+
+// var myStr = "my name is noila." + "my surname is ibodullayeva.";
+// console.log(myStr);
+
+// var myTsr = "this is a first sentences "
+// myTsr += " this is a second sentences"
+// console.log(myTsr);
+
+// var MyName = "noila";
+// var MyTsr = "my name is " + MyName + " and i am a student";
+// console.log(MyTsr);
+
+// var someAdjective = "beautiful";
+// var str = " i am  ";
+// str += someAdjective;
+// console.log(str);
+
+// var lastNameLenght = "";
+// var lastName = "lovelace"
+// // lastNameLenght = lastName.length;
+// // console.log(lastNameLenght);
+// lastNameLenght = lastName[0];
+// console.log(lastNameLenght);
+
+// var kkm = "jello world";
+// kkm = "hello world";
+// console.log(kkm);
+
+// function wordBlanks(myNoun, myAdjective, myVerb, myAdverb){
+//   var result ="";
+//   result += "the " +  myAdjective + " " + myNoun + " " + myVerb + " to the store " +  myAdverb;
+//   return result;
+// }
+// console.log(wordBlanks("dog", "big", "run", "quickly"));
+
+
+// var myGlobal = 10;
+// function fun1(){
+
+// }
+// function fun2(){
+//   var output ="";
+//   if (typeof myGlobal != "undefined"){
+//     output += "myGlobal: " + myGlobal;
+//   }
+//   if (typeof oopsGlobal != "undefined"){
+//     output += "oopsGlobal: " + oopsGlobal;
+//     }
+//     console.log(output)
+// }
+// fun1();
+// fun2();
+
+// function TruFalse (whatDoYouThinkIsItTue){
+//   if (whatDoYouThinkIsItTue) {
+//     return "yes true";
+//   }
+//  return "no false";
+// }
+// console.log(TruFalse(false));
+// console.log(TruFalse(true));
+// console.log(TruFalse(false));
+// console.log(TruFalse(true));
+
+
+
+// function defaultAnswer(val){
+//   var answer = "";
+//   switch (val) {
+//     case "a":
+//       answer = "apple";
+//       break;
+//     case "b":
+//       answer = "bird";
+//       break;
+//     case "c":
+//       answer = "cat";
+//       break;
+//     default:
+//       answer = " stuff";
+//       break;
+      
+//   }
+//   return answer;
+// }
+// console.log(defaultAnswer(2));
+
+
